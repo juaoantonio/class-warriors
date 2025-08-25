93cdd8cbf34f1754b011d6b935d3ba168154d920
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-08-24 21:28:44 -0300

    refactor: remove unused methods

--- src/main/java/br/dev/joaobarbosa/domain/logs/BattleLog.java
+++ src/main/java/br/dev/joaobarbosa/domain/logs/BattleLog.java
@@ -2,22 +2,9 @@ package br.dev.joaobarbosa.domain.logs;
 
 import java.util.List;
 
-public class BattleLog {
-  private final List<BattleLogEntry> entries;
+public record BattleLog(List<BattleLogEntry> entries) {
 
-  public BattleLog(List<BattleLogEntry> entries) {
-    this.entries = entries;
-  }
-
-  public void addEntry(BattleLogEntry entry) {
-    entries.add(entry);
-  }
-
-  public List<BattleLogEntry> getEntries() {
+  public List<BattleLogEntry> entries() {
     return List.copyOf(entries);
-  }
-
-  public void print() {
-    entries.forEach(System.out::println);
   }
 }
